8add9d857f2daa27da20bdef48fb18202db988ea
tshakitit <104494470+tshakitit@users.noreply.github.com>
2023-03-06 22:38:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 git clone https://github.com/kenzok8/luci-theme-ifit.git package/luci-theme-ifit
